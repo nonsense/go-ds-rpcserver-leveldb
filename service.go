356a5cea4fb181676592aa6b2833706d34c71841
Anton Evangelatov <anton.evangelatov@gmail.com>
2022-05-06 14:12:22 +0300

    impl more methods

--- service.go
+++ service.go
@@ -19,6 +19,7 @@ type DatastoreService struct {
 }
 
 func NewDatastoreService() *DatastoreService {
+	//TODO: add config and options
 	db, err := levelDs("/tmp/smth", false)
 	if err != nil {
 		panic(err)
@@ -82,18 +83,51 @@ func (s *DatastoreService) Put(key ds.Key, value []byte) error {
 	return s.db.Put(ctx, key, value)
 }
 
-//func (s *DatastoreService) Delete(r *http.Request, req *DeleteRequest, res *DeleteResponse) error {
-//panic("delete - not implemented")
-//}
+func (s *DatastoreService) Delete(key ds.Key) error {
+	log.Debugw("handle.delete", "key", key)
 
-//func (s *DatastoreService) Sync(r *http.Request, req *SyncRequest, res *SyncResponse) error {
-//panic("sync - not implemented")
-//}
+	defer func(now time.Time) {
+		log.Debugw("handled.delete", "took", fmt.Sprintf("%s", time.Since(now)))
+	}(time.Now())
 
-//func (s *DatastoreService) Close(r *http.Request, req *CloseRequest, res *CloseResponse) error {
-//panic("close - not implemented")
-//}
+	ctx := context.TODO()
 
-//func (s *DatastoreService) Batch(r *http.Request, req *BatchRequest, res *BatchResponse) error {
-//panic("batch - not implemented")
-//}
+	return s.db.Delete(ctx, key)
+}
+
+func (s *DatastoreService) Sync(prefix ds.Key) error {
+	log.Debugw("handle.sync", "prefix", prefix)
+
+	defer func(now time.Time) {
+		log.Debugw("handled.sync", "took", fmt.Sprintf("%s", time.Since(now)))
+	}(time.Now())
+
+	ctx := context.TODO()
+
+	return s.db.Sync(ctx, prefix)
+}
+
+func (s *DatastoreService) Close() error {
+	log.Debugw("handle.close")
+
+	defer func(now time.Time) {
+		log.Debugw("handled.close", "took", fmt.Sprintf("%s", time.Since(now)))
+	}(time.Now())
+
+	return s.db.Close()
+}
+
+//// Batching iface
+
+//TODO: not sure if ds.Batch will be serialzed correctly
+func (s *DatastoreService) Batch() (ds.Batch, error) {
+	log.Debugw("handle.batch")
+
+	defer func(now time.Time) {
+		log.Debugw("handled.batch", "took", fmt.Sprintf("%s", time.Since(now)))
+	}(time.Now())
+
+	ctx := context.TODO()
+
+	return s.db.Batch(ctx)
+}
